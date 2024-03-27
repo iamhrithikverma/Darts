20db13c6a548a1a7dafe992f768ab8f3eff7e3f7
Hrithik Verma <152607244+iamhrithikverma@users.noreply.github.com>
2024-03-27 11:39:05 +0530

    Create MainColor.dart

---
+++ Tic-Tac-Toe/MainColor.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color(0xFFC5A98F);
+  // static Color primaryColor = Color(0xFF00061a);
+  static Color secondaryColor = Color(0xFF776D4F);
+  static Color accentColor = Color(0xFF966659);
+}
